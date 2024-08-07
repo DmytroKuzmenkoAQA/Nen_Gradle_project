9b3505e38f98a76517c57c90cb4a6b1ac0f4b16f
IS.dmytro.kuzmenko <maximus19919@gmail.com>
2024-08-07 22:51:09 +0200

    Singleton file

---
+++ src/test/java/OrgExample/SingletonDriver.java
@@ -0,0 +1,21 @@
+package OrgExample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SingletonDriver {
+    private static SingletonDriver INSTANCE;
+    private WebDriver driver;
+    private SingletonDriver() {
+        driver = new ChromeDriver();
+    }
+    public static SingletonDriver getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new SingletonDriver();
+        }
+        return INSTANCE;
+    }
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
